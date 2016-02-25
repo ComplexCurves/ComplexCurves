7461e9229410f25787a1993a114394c1c27cf2cd
Stefan Kranich <stefan.kranich@ma.tum.de>
2016-02-25 16:19:07 +0100

    Fix spurious rendering bug

--- src/js/Mesh.js
+++ src/js/Mesh.js
@@ -6,7 +6,7 @@ Mesh.tetrakis = function(n) {
     var mesh = [];
     for (var x = -5 * n; x < 5 * n; x++) {
         for (var y = -5 * n; y < 5 * n; y++) {
-            if (x + y % 2 === 0) {
+            if (x + y % 2 !== 0) {
                 mesh = mesh.concat(
                     [x, y, x + 1, y, x, y + 1, x + 1, y, x + 1, y + 1, x, y + 1]
                 );
