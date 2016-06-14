0052cfa571bad2104ede37a83ea6b233b1c831f0
Stefan Kranich <stefan.kranich@ma.tum.de>
2016-06-14 10:53:39 +0200

    First steps towards deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+name=$(git describe --always)
+rm -rf build/deploy
+mkdir -p build/deploy
+git clone --depth 1 . build/deploy || exit 1
+(
+    cd build/deploy
+    git fetch origin gh-pages
+    git checkout FETCH_HEAD
+    git checkout -b gh-pages
+    rm -rf * .gitignore .jshintrc .travis.yml
+    mkdir build
+    cp ../ComplexCurves.js build/ComplexCurves.js
+    cp ../ComplexCurves.js.map build/ComplexCurves.js.map
+    cp ../../examples.json ./examples.json
+    cp ../../favicon.ico ./favicon.ico
+    cp -R ../../images ./images
+    cp ../../index.css ./index.css
+    cp ../../index.html ./index.html
+    cp ../../index.js ./index.js
+    cp -R ../../models ./models
+    mkdir lib
+    cp -R ../../node_modules/jquery lib/jquery
+    cp -R ../../node_modules/semantic-ui-css lib/semantic-ui-css
+    sed -i 's/node_modules/lib/g' index.html
+    git add -A
+    git config user.name "Travis CI"
+    git config user.email "travis-ci@complexcurves.org"
+    git commit -m "Deployment of Complex Curves ${name}"
+    git push git@github.com:kranich/ComplexCurves.git gh-pages:gh-pages
+)
