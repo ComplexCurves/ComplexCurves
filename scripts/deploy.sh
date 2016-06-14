88a2a6d45ddb9a454876577ec82161c3fd229fb7
Stefan Kranich <stefan.kranich@ma.tum.de>
2016-06-14 12:31:56 +0200

    Let scripts/deploy.sh add README.md

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,7 @@ git clone --depth 1 . build/deploy || exit 1
     git checkout -b gh-pages
     rm -rf * .gitignore .jshintrc .travis.yml
     echo "complexcurves.org" >CNAME
+    cp ../../README.md ./README.md
     mkdir build
     cp ../ComplexCurves.js build/ComplexCurves.js
     cp ../ComplexCurves.js.map build/ComplexCurves.js.map
