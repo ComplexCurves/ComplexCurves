b045e5b496ea0f186aa77f414ce8c0970d555a8f
Stefan Kranich <stefan.kranich@ma.tum.de>
2017-02-27 20:17:42 +0100

    Partly migrate SubdivisionPre stage from textures to transform feedback

--- src/glsl/SubdivisionPre.vert
+++ src/glsl/SubdivisionPre.vert
@@ -4,13 +4,39 @@ precision highp float;
 precision mediump float;
 #endif
 in float index;
-out vec2 texCoord[2];
+uniform sampler2D samplers[1 + N/2];
+vec2 texCoord[2];
+out vec2 position;
+out float delta;
+out float subdivisionFlag;
+out vec2 values[N];
+
+#define W 2048.0
+#define H 2048.0
+vec2 uvPosition (in float i) {
+    return vec2 (mod (i, W) / W + 0.5 / W, floor (i / W) / H + 0.5 / H);
+}
+
+bool validOutgoingEdge () {
+    vec3 next = texture (samplers[0], texCoord[1]).xyz;
+    return distance (position, next.xy) < min (delta, next.z);
+}
 
 void main (void) {
-    gl_Position = indexedPosition (index);
     float j = 1.0;
     if (mod (index, 3.0) > 1.0)
         j = -2.0;
     texCoord[0] = uvPosition (index);
     texCoord[1] = uvPosition (index + j);
+    vec4 s = texture (samplers[0], texCoord[0]);
+    position = s.xy;
+    delta = s.z;
+    subdivisionFlag = s.w;
+    if (subdivisionFlag == 1.0 && validOutgoingEdge ())
+        subdivisionFlag = 0.0;
+    for (int i = 1; i < N/2; i++) {
+        s = texture (samplers[i], texCoord[0]);
+        values[2*i-2] = s.xy;
+        values[2*i-1] = s.zw;
+    }
 }
--- src/js/SubdivisionPre.js
+++ src/js/SubdivisionPre.js
@@ -13,9 +13,14 @@ function SubdivisionPre(stategl, surface) {
  * @param {Surface} surface
  */
 SubdivisionPre.prototype.mkProgram = function(stategl, surface) {
-    var sources = StateGL.getShaderSources("SubdivisionPre");
-    sources[0] = surface.withTextures(sources[0]);
-    this.program = stategl.mkProgram(sources);
+    var sources = [
+        StateGL.getShaderSource('SubdivisionPre.vert'),
+        StateGL.getShaderSource('Dummy.frag')
+    ];
+    sources[0] = surface.withCustomAndCommon(sources[0]);
+    this.program = stategl.mkProgram(sources, ["position", "delta",
+        "subdivisionFlag", "values"
+    ]);
 };
 
 /** @type {WebGLProgram} */
@@ -27,35 +32,48 @@ SubdivisionPre.prototype.program = null;
  * @param {WebGLRenderingContext} gl
  */
 SubdivisionPre.prototype.render = function(stategl, surface, gl) {
-    var textureIn = surface.texturesIn[0],
-        textureOut = surface.texturesOut[0];
-    gl.useProgram(this.program);
+    var texturesIn = surface.texturesIn,
+        texturesOut = surface.texturesOut;
+    var program = this.program;
+    gl.useProgram(program);
     surface.fillIndexBuffer(stategl);
     gl.vertexAttribPointer(0, 1, gl.FLOAT, false, 0, 0);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, surface.framebuffer);
-    gl.bindTexture(gl.TEXTURE_2D, textureOut);
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D,
-        textureOut, 0);
-    gl.bindTexture(gl.TEXTURE_2D, null);
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, textureIn);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    var samplerLocation = gl.getUniformLocation(this.program, 'sampler');
-    gl.uniform1i(samplerLocation, 0);
-    gl.disable(gl.DEPTH_TEST);
-    gl.viewport(0, 0, 2048, 2048);
-    gl.drawArrays(gl.POINTS, 0, surface.numIndices);
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D,
-        null, 0);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    var stride = 4 + 2 * GLSL.N;
+    var size = stride * surface.numIndices;
+    gl.bindBuffer(gl.ARRAY_BUFFER, surface.transformFeedbackBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, size * Float32Array.BYTES_PER_ELEMENT, gl["STATIC_COPY"]);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    var textureTmp = textureIn;
-    surface.texturesIn[0] = textureOut;
-    surface.texturesOut[0] = textureTmp;
+    // prepare input textures
+    var texIs = [];
+    for (var i = 0, l = texturesIn.length; i < l; i++) {
+        gl.activeTexture(gl.TEXTURE0 + i);
+        gl.bindTexture(gl.TEXTURE_2D, texturesIn[i]);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        texIs[i] = i;
+    }
+    var samplersLocation = gl.getUniformLocation(program, 'samplers');
+    gl.uniform1iv(samplersLocation, texIs);
+
+    gl["bindTransformFeedback"](gl["TRANSFORM_FEEDBACK"], surface.transformFeedback);
+    gl["bindBufferBase"](gl["TRANSFORM_FEEDBACK_BUFFER"], 0, surface.transformFeedbackBuffer);
+    gl["beginTransformFeedback"](gl.POINTS);
+    gl.drawArrays(gl.POINTS, 0, surface.numIndices);
+    gl["endTransformFeedback"]();
+    gl["bindBufferBase"](gl["TRANSFORM_FEEDBACK_BUFFER"], 0, null);
+    gl["bindTransformFeedback"](gl["TRANSFORM_FEEDBACK"], null);
+
+    // store feedback values in textures
+    TransformFeedback.toTextures(gl, surface, texturesOut);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var texturesTmp = texturesIn;
+    surface.texturesIn = texturesOut;
+    surface.texturesOut = texturesTmp;
 };
 
 /** @type {number} */
