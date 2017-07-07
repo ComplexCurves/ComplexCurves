f25d2026e3583a88cb1393d9802cfbc22dd9dcb5
Stefan Kranich <stefan.kranich@ma.tum.de>
2017-07-07 14:01:25 +0200

    Migrate compilation and generation of sourcemaps to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const beautify = require('gulp-beautify');
 const concat = require('gulp-concat');
+const compilerPackage = require('google-closure-compiler');
+const closureCompiler = compilerPackage.gulp();
 const del = require('del');
 const gap = require('gulp-append-prepend');
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const jsstring = require('gulp-js-string');
+const sourcemaps = require('gulp-sourcemaps');
 
 const paths = {
     js: ['index.js', 'src/js/*.js', 'gulpfile.js'],
@@ -44,4 +47,26 @@ gulp.task('resources', function() {
         .pipe(gulp.dest('build/'));
 });
 
-gulp.task('default', ['jshint', 'beautify']);
+gulp.task('js-compile', ['jshint', 'beautify', 'resources'], function() {
+    return gulp.src(['build/resources.js', 'src/js/*.js'], {
+            base: './'
+        })
+        .pipe(sourcemaps.init())
+        .pipe(closureCompiler({
+            language_in: 'ECMASCRIPT6_STRICT',
+            language_out: 'ECMASCRIPT5_STRICT',
+            dependency_mode: 'LOOSE',
+            module_resolution: 'NODE',
+            compilation_level: 'ADVANCED',
+            warning_level: 'VERBOSE',
+            jscomp_warning: 'reportUnknownTypes',
+            rewrite_polyfills: false,
+            output_wrapper_file: 'src/js/ComplexCurves.js.wrapper',
+            summary_detail_level: '3',
+            js_output_file: 'ComplexCurves.js',
+        }))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('default', ['js-compile']);
