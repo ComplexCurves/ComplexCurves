c1b0ae5486d60662513e0a39c5c0dc909399f057
Stefan Kranich <stefan.kranich@andrena.de>
2017-12-04 14:22:35 +0100

    Remove website sources

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,24 +8,8 @@ mkdir -p build/deploy
 git clone --depth 1 git@github.com:ComplexCurves/website.git build/deploy || exit 1
 (
     cd build/deploy
-    mv ./models ../models
-    rm -rf * .gitignore .jshintrc .travis.yml .travis
-    echo "complexcurves.org" >CNAME
-    cp ../../README.md ./README.md
-    mkdir build
     cp ../ComplexCurves.js build/ComplexCurves.js
     cp ../ComplexCurves.js.map build/ComplexCurves.js.map
-    cp ../../examples.json ./examples.json
-    cp ../../favicon.ico ./favicon.ico
-    cp -R ../../images ./images
-    cp ../../index.css ./index.css
-    cp ../../index.html ./index.html
-    cp ../../index.js ./index.js
-    mv ../models ./models
-    mkdir lib
-    cp -R ../../node_modules/jquery lib/jquery
-    cp -R ../../node_modules/semantic-ui-css lib/semantic-ui-css
-    sed -i 's/node_modules/lib/g' index.html
     git add -A
     git config user.name "Travis CI"
     git config user.email "travis-ci@complexcurves.org"
