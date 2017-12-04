deefef02a3d8e81a6b68756e2779c56c875fde07
Stefan Kranich <stefan.kranich@andrena.de>
2017-12-04 13:42:38 +0100

    Deploy to ComplexCurves/website master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,9 @@ chmod 600 .travis/github_deploy_key
 ssh-add .travis/github_deploy_key
 rm -rf build/deploy
 mkdir -p build/deploy
-git clone --depth 1 git@github.com:ComplexCurves/ComplexCurves.git build/deploy || exit 1
+git clone --depth 1 git@github.com:ComplexCurves/website.git build/deploy || exit 1
 (
     cd build/deploy
-    git fetch origin gh-pages
-    git checkout FETCH_HEAD
-    git checkout -b gh-pages
     mv ./models ../models
     rm -rf * .gitignore .jshintrc .travis.yml .travis
     echo "complexcurves.org" >CNAME
@@ -33,5 +30,5 @@ git clone --depth 1 git@github.com:ComplexCurves/ComplexCurves.git build/deploy 
     git config user.name "Travis CI"
     git config user.email "travis-ci@complexcurves.org"
     git commit -m "Deployment of Complex Curves ${name}"
-    git push git@github.com:ComplexCurves/ComplexCurves.git gh-pages:gh-pages
+    git push git@github.com:ComplexCurves/website.git master:master
 )
