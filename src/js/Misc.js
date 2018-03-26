0a3880470f369e9cefbeac690775df7be8d37991
Stefan Kranich <stefan.kranich@ma.tum.de>
2018-03-26 19:12:20 +0200

    Remove unused function Misc.loadTextFiles

--- src/js/Misc.js
+++ src/js/Misc.js
@@ -8,24 +8,4 @@ module.exports = class Misc {
     static lerp(x, y, mu) {
         return x + mu * (y - x);
     }
-
-    /**
-     * @param {Array<string>} files
-     * @param {function(Array<string>)} onload
-     */
-    static loadTextFiles(files, onload) {
-        const sources = [];
-        let count = 0;
-        files.forEach(function(file, i, files) {
-            const req = new XMLHttpRequest();
-            req.open("GET", file, true);
-            req.responseType = "text";
-            req.onload = function() {
-                sources[i] = req.responseText;
-                if (++count === files.length)
-                    onload(sources);
-            };
-            req.send();
-        });
-    }
 };
