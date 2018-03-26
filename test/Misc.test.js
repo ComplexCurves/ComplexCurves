94d35e5ac7cb013d0dec2d3fd054d539dc3284ca
Stefan Kranich <stefan.kranich@ma.tum.de>
2018-03-26 19:12:57 +0200

    Beautify

--- test/Misc.test.js
+++ test/Misc.test.js
@@ -4,7 +4,8 @@ const Misc = require('../src/js/Misc.js');
 describe('Misc', function() {
     describe('lerp', function() {
         it('interpolates linearly between two numbers', function() {
-            const x = 2, y = 3;
+            const x = 2,
+                y = 3;
             expect(Misc.lerp(x, y, 0)).to.equal(x);
             expect(Misc.lerp(x, y, 0.5)).to.equal((x + y) / 2);
             expect(Misc.lerp(x, y, 1)).to.equal(y);
