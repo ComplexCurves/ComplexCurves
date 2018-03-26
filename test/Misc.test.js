70cac2b471f4ceb7bf2774c70f78a582a823d3e8
Stefan Kranich <stefan.kranich@ma.tum.de>
2018-03-26 19:12:33 +0200

    Add tests for Misc.lerp

---
+++ test/Misc.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const Misc = require('../src/js/Misc.js');
+
+describe('Misc', function() {
+    describe('lerp', function() {
+        it('interpolates linearly between two numbers', function() {
+            const x = 2, y = 3;
+            expect(Misc.lerp(x, y, 0)).to.equal(x);
+            expect(Misc.lerp(x, y, 0.5)).to.equal((x + y) / 2);
+            expect(Misc.lerp(x, y, 1)).to.equal(y);
+        });
+    });
+});
